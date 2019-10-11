506560921f2d3d5a30278e0b8436664910692b0e
Rafael Dalsenter <rafaeldalsenter@gmail.com>
2019-10-11 19:33:18 -0300

    Criado projeto de teste do MS a ser acessado

---
+++ Ocelot.Example/Ocelot.Example.MsDePrecoDoProduto/Controllers/PrecoDoProdutoController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Ocelot.Example.MsDePrecoDoProduto.Dtos;
+
+namespace Ocelot.Example.MsDePrecoDoProduto.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PrecoDoProdutoController : ControllerBase
+    {
+        private static readonly string[] _nomesDeProdutos = new[]
+        {
+           "Coca-cola",
+           "Pepsi",
+           "Fandangos",
+           "Teclado",
+           "Saco de carvão"
+        };
+
+        private readonly ILogger<PrecoDoProdutoController> _logger;
+
+        public PrecoDoProdutoController(ILogger<PrecoDoProdutoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public PrecoDoProduto Get()
+        {
+            var rng = new Random();
+
+            return new PrecoDoProduto
+            {
+                IdDoProduto = rng.Next(1, 1000),
+                NomeDoProduto = _nomesDeProdutos[rng.Next(_nomesDeProdutos.Length)],
+                Preco = rng.Next(10, 20)
+            };
+        }
+    }
+}
---
+++ Ocelot.Example/Ocelot.Example.MsDePrecoDoProduto/Dtos/PrecoDoProduto.cs
@@ -0,0 +1,9 @@
+﻿namespace Ocelot.Example.MsDePrecoDoProduto.Dtos
+{
+    public class PrecoDoProduto
+    {
+        public int IdDoProduto { get; set; }
+        public string NomeDoProduto { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
