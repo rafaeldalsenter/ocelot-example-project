dfe62a97383e97fad51288e66a259175584b66b3
Rafael Dalsenter <rafaeldalsenter@gmail.com>
2019-10-16 21:08:00 -0300

    Criação de um projeto simulando uma api de autenticação

---
+++ Ocelot.Example/Ocelot.Example.MsAuth/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Ocelot.Example.MsAuth.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly ILogger<AuthController> _logger;
+
+        public AuthController(ILogger<AuthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public bool Get()
+        {
+            return false;
+        }
+
+        [HttpPost]
+        public void Post()
+        {
+        }
+    }
+}
